083d0177d2b5f7263e66677599383492cdb13dcd
Milan <milan@uvid.hr>
2018-08-06 11:18:31 +0200

    Init commit 2

---
+++ addons/training/__manifest__.py
@@ -0,0 +1,26 @@
+
+{
+    'name' : 'Training',
+    'version' : '1.0',
+    'summary': 'Training',
+    'sequence': 30,
+    'description': """
+Training
+""",
+    'category': 'Other',
+    'website': '',
+    'images' : [],
+    'depends' : ['base'],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'qweb': [
+
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}

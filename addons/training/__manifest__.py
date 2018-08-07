d4261e99a9411caa986ae6dc608ca4bcfec4b916
Milan <milan@uvid.hr>
2018-08-07 14:22:40 +0200

    Obuka 1. dan

--- addons/training/__manifest__.py
+++ addons/training/__manifest__.py
@@ -1,26 +1,34 @@
-
+# -*- coding: utf-8 -*-
 {
-    'name' : 'Training',
-    'version' : '1.0',
-    'summary': 'Training',
-    'sequence': 30,
+    'name': "training",
+
+    'summary': """
+        Training module for AKD""",
+
     'description': """
-Training
-""",
-    'category': 'Other',
-    'website': '',
-    'images' : [],
-    'depends' : ['base'],
+        Long description for training module for AKD
+    """,
+
+    'author': "Uvid",
+    'website': "http://www.odoo.com.hr",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
     'data': [
-
+        # 'security/ir.model.access.csv',
+        'views/training_views.xml',
+        # 'views/templates.xml',
     ],
+    # only loaded in demonstration mode
     'demo': [
-
+        # 'demo/demo.xml',
     ],
-    'qweb': [
-
-    ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
 }
---
+++ addons/training/models/training.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+class trainingTraining(models.Model):
+    _name = 'training.training'
+
+    name = fields.Char('Name', limit=20, required=True)
+    description = fields.Text('Description')
+    length_days = fields.Integer('Length In Days')
+    start_date = fields.Date('Start Date')
+    price = fields.Float('Training Price', digits=(16, 4))
+    # create_date = fields.Datetime('Created On', readonly=True)
+    state = fields.Selection([('draft', 'Draft'),
+                              ('confirmed', 'Confirmed'),
+                              ('done', 'Done'),
+                              ('cancel', 'Cancelled')], default='draft')
+    student_ids = fields.One2many('training.student', 'training_id', 'Students')
+    trainer_id = fields.Many2one('res.partner', 'Trainer', required=True)
+
+    @api.multi
+    def action_confirm(self):
+        for training in self:
+            training.state = 'confirmed'
+
+    @api.multi
+    def action_done(self):
+        for training in self:
+            training.state = 'done'
+
+    @api.multi
+    def action_cancel(self):
+        for training in self:
+            training.state = 'cancel'
+
+    @api.multi
+    def action_activate(self):
+        for training in self:
+            training.state = 'draft'
+
+    @api.multi
+    def unlink(self):
+        # for training in self:
+        #     if training.state != 'cancel':
+        #         raise UserError(_('You cannot delete training that are not canceled!'))
+
+        if self.filtered(lambda x: x.state not in ('cancel')):
+            raise UserError(_('You cannot delete training that are not canceled!'))
+
+        return super(trainingTraining, self).unlink()
+
+
+    # value = fields.Integer()
+    # value2 = fields.Float(compute="_value_pc", store=True)
+
+    # @api.depends('value')
+    # def _value_pc(self):
+    #     self.value2 = float(self.value) / 100
+
+
+class studentTraining(models.Model):
+    _name = 'training.student'
+
+    training_id = fields.Many2one('training.training', 'Training', required=True, readonly=True)
+    name = fields.Char('Name', limit=20, required=True)
+    surname = fields.Char('Surname', limit=30, required=True)
+    age = fields.Integer('Age')
